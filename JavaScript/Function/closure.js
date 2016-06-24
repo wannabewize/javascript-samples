df6c5056ee1a4fc83f684bdc0d11874490487057
wannabewize <wannabewize+macbookpro@gmail.com>
2016-06-24 18:01:33 +0900

    자바 스크립트 예제

--- JavaScript/Function/closure.js
+++ JavaScript/Function/closure.js
@@ -1,7 +1,33 @@
+function makeRandom(to) {
+   return Math.floor( Math.random() * to )
+}
+
+console.log('makeRandom(10) : ',makeRandom(10));
+
+function makeRandomFn(to) {
+   function fn() {
+      return Math.floor( Math.random() * to );
+   }
+   return fn;
+}
+
+var random10 = makeRandomFn(10);
+console.log('random10 : ', random10());
+
+var random100 = makeRandomFn(100);
+console.log('random100 : ', random100());
+
+
 function makeId() {
    var lastId = 0;
+
+   function fn() {
+      return ++lastId;
+   }
    
-   return function() {return ++lastId};
+   return function(){
+      return ++lastId
+   };
 }
 
 var idFunc = makeId();
---
+++ JavaScript/Function/function.js
@@ -0,0 +1,18 @@
+function func1(a, b) {
+    return a * b;
+}
+ var ret = func1(4, 3);
+ console.log(ret);
+
+var func2 = function (a, b) {
+	return a * b
+};
+var ret2 = func2(4, 3);
+console.log(ret2);
+
+var ret3 = func3(3, 3);
+console.log(ret3);
+
+function func3(a, b) {
+    return a * b;
+}
