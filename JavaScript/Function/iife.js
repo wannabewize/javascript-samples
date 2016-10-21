0e21358d7751c58d017d0b8a5eae38ec9cf73fbb
wannabewize <wannabewize+macbookpro@gmail.com>
2016-10-21 21:21:12 +0900

    git repository 재조정

--- JavaScript/Function/iife.js
+++
@@ -1,9 +0,0 @@
-// IIFE : Immediately Invoked Function Expression
-(function(){
-   console.log('IIFE Test1');
-})();
-
-// with Parameter
-(function(arg){
-   console.log('IIFE Test2 - argumanet : ', arg.name);
-})({'name':'value'});
--- JavaScript/Function/innerFunction.js
+++
@@ -1,9 +0,0 @@
-function circle(radius) {
-    var pi = 3.14;
-    function area(r) {
-        return r * r * pi;
-    }
-    return area(radius);
-}
-var ret = circle(3);
-console.log(ret);
