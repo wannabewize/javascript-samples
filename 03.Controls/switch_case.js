8880008a14480d97eae96887bc124b5a99d3105a
Jaehoon Lee <wannabewize@gmail.com>
2017-04-19 00:37:42 +0900

    switch-case

--- 03.Controls/switch_case.js
+++ 03.Controls/switch_case.js
@@ -3,3 +3,14 @@
  */
 
 var value = 2;
+
+switch (value) {
+    case 1:
+        console.log('switch-case value=1');
+        break;
+    case 2, 3, 4:
+        console.log('switch-case 2, 3, 4');
+        break;
+    default:
+        console.log('default:')
+}
