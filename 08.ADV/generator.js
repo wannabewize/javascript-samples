437db55bc6cba443203a9296c4041e82fdd72e12
wannabewize <wannabewize@gmail.com>
2019-03-22 04:36:59 +0900

    generator 예제

---
+++ 08.ADV/generator.js
@@ -0,0 +1,19 @@
+function* gen() {
+    yield 1;
+    yield 2;
+    yield 3;
+}
+
+const g = gen();
+
+
+for (const item of g) {
+    console.log(item);
+}
+
+const k = gen();
+
+console.log(k);
+console.log(k.next());
+console.log(k.next());
+console.log(k.next());
